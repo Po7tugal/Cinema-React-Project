387e7ffc69061ce1091679f4a6bb22a1e77318a2
Filipe <a14812@oficina.pt>
2026-05-27 11:04:26 +0000

    Inicialização

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="Header">
+      <nav className="nav">
+        <h1>CINEMAX</h1>
+        <ul className="nav-list">
+          <li className="nav-item"><a href="#">Home</a></li>
+          <li className="nav-item"><a href="Catalog">Catalog</a></li>
+        </ul>
+      </nav>
+    </div>
+    </>
+  )
+}
+
+export default App
